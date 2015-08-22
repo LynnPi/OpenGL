ba2b58aabe0a359a480d922d1c2907c899a48cea
LynnPi <18681276745@163.com>
2015-08-22 17:33:54 +0800

    Add DrawPloygon

--- Project/OpenGL_Demo/OpenGL_Demo/FirstDemo.cpp
+++ Project/OpenGL_Demo/OpenGL_Demo/FirstDemo.cpp
@@ -65,13 +65,33 @@ void DrawSinCurve(void){
 
 }
 
+void DrawPloygon(void){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPolygonMode(GL_FRONT, GL_FILL);//设置正面为填充模式
+	glPolygonMode(GL_BACK, GL_LINE);//设置反面为线形模式
+	glFrontFace(GL_CCW);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.0f, -0.5f);
+	glVertex2f(0.0f, 0.0f);
+	glVertex2f(-0.5f, 0.0f);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex2f(0.0f, 0.0f);
+	glVertex2f(0.0f, 0.5f);
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(0.5f, 0.0f);
+	glEnd();
+	glFlush();
+}
+
 int main(int argc, char *argv[]){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
 	glutInitWindowPosition(0, 0);
 	glutInitWindowSize(800, 800);
 	glutCreateWindow("第一个OpenGL程序");
-	glutDisplayFunc(&DrawMyFiveAngleStar);
+	glutDisplayFunc(&DrawPloygon);
 	glutMainLoop();
 	return 0;
 }
